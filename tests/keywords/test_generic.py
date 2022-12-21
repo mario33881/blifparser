b587f5394b8e9578bcd668514a5c4ea2a20bab0f
Stefano Zenaro <mariortgasd@hotmail.com>
2022-12-21 21:40:33 +0100

    + some tests

--- tests/keywords/test_generic.py
+++ tests/keywords/test_generic.py
@@ -10,25 +10,120 @@ import generic  # noqa: E402, F401
 
 
 class TestGeneric(unittest.TestCase):
-    @unittest.skip("TODO: write Model() tests")
+    
     def test_model(self):
-        pass
+        """
+        Tests the Model() class which represents the .model keyword in BLIF files.
+        """
+        with self.assertRaises(TypeError):
+            generic.Model(None)
+        
+        with self.assertRaises(ValueError):
+            generic.Model("")
+        
+        with self.assertRaises(ValueError):
+            generic.Model(" ")
+        
+        with self.assertRaises(ValueError):
+            generic.Model("too many parameters")
+        
+        model = generic.Model(" valid_name ")
+        self.assertEqual(model.name, "valid_name")
 
-    @unittest.skip("TODO: write Inputs() tests")
     def test_inputs(self):
-        pass
+        """
+        Tests the Inputs() class which represents the .inputs keyword in BLIF files.
+        """
+        with self.assertRaises(TypeError):
+            generic.Inputs(None)
+        
+        with self.assertRaises(ValueError):
+            generic.Inputs("")
+        
+        with self.assertRaises(ValueError):
+            generic.Inputs(" ")
+        
+        inputs = generic.Inputs(" valid number of parameters ")
+        self.assertEqual(inputs.inputs, ["valid", "number", "of", "parameters"])
 
-    @unittest.skip("TODO: write Outputs() tests")
     def test_outputs(self):
-        pass
+        """
+        Tests the Outputs() class which represents the .outputs keyword in BLIF files.
+        """
+        with self.assertRaises(TypeError):
+            generic.Outputs(None)
+        
+        with self.assertRaises(ValueError):
+            generic.Outputs("")
+        
+        with self.assertRaises(ValueError):
+            generic.Outputs(" ")
+        
+        outputs = generic.Outputs(" valid number of parameters ")
+        self.assertEqual(outputs.outputs, ["valid", "number", "of", "parameters"])
 
-    @unittest.skip("TODO: write Names() tests")
     def test_names(self):
-        pass
+        """
+        Tests the Names() class which represents the .names keyword in BLIF files.
 
-    @unittest.skip("TODO: write Latch() tests")
+        TODO: test truth tables
+        """
+        with self.assertRaises(TypeError):
+            generic.Names(None, False)
+        
+        with self.assertRaises(TypeError):
+            generic.Names("", None)
+        
+        with self.assertRaises(ValueError):
+            generic.Names("", False)
+        
+        with self.assertRaises(ValueError):
+            generic.Names(" ", False)
+        
+        # one parameter: 0 inputs, 1 output
+        names1 = generic.Names(" constant ", False)
+        self.assertEqual(names1.inputs, [])
+        self.assertEqual(names1.output, "constant")
+        self.assertFalse(names1.is_dontcare)
+
+        # multiple parameters: x inputs, 1 output
+        names2 = generic.Names(" valid number of parameters ", False)
+        self.assertEqual(names2.inputs, ["valid", "number", "of"])
+        self.assertEqual(names2.output, "parameters")
+        self.assertFalse(names2.is_dontcare)
+
     def test_latch(self):
-        pass
+        """
+        Tests the Latch() class which represents the .latch keyword in BLIF files.
+        """
+        with self.assertRaises(TypeError):
+            generic.Latch(None)
+        
+        # not enough parameters
+        with self.assertRaises(Exception):
+            generic.Latch(" ")
+        
+        with self.assertRaises(Exception):
+            generic.Latch(" a ")
+        
+        # too many parameters
+        with self.assertRaises(Exception):
+            generic.Latch(" too many parameters for this component ")
+        
+        # wrong type
+        with self.assertRaises(ValueError):
+            generic.Latch(" input output wrong_type control initval ")
+        
+        # wrong initval
+        with self.assertRaises(ValueError):
+            generic.Latch(" input output re control wrong_initval ")
+
+        latch1 = generic.Latch(" input output re control 0 ")
+        self.assertEqual(latch1.input, "input")
+        self.assertEqual(latch1.output, "output")
+        self.assertEqual(latch1.type, "re")
+        self.assertEqual(latch1.control, "control")
+        self.assertEqual(latch1.initval, "0")
 
     @unittest.skip("TODO: write Blif() tests")
     def test_blif(self):
